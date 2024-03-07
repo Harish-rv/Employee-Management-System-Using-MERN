960b3598b319a1fd3614e06325cc1eb0063a2d48
Harish-rv <harishrvs1@gmail.com>
2024-03-08 00:21:58 +0530

    port text changed

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ app.use('/admin',require('./server/routes/AdminRouter'));
 
 connectDB();
 app.listen(port,()=>{
-    console.log('listening post runng is on '+port);
+    console.log('listening post is running  on '+port);
 });
